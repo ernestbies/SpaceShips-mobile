38aeeed61552fb2c60818d37d5b2ccb74efdd34e
ernestbies <ernestbies@gmail.com>
2019-11-24 11:18:48 +0100

    Added descriptions of methods in class - Ship.java

--- server/src/main/java/com/shipsgame/domain/model/Ship.java
+++ server/src/main/java/com/shipsgame/domain/model/Ship.java
@@ -6,35 +6,42 @@ import java.util.ArrayList;
 
 public class Ship implements Serializable {
 
-    private int type;
-    private String name;
-    private List<String> positions = new ArrayList<>();
+    private int type; //type of spaceship
+    private String name; //name of the ship
+    private List<String> positions = new ArrayList<>(); //ship positions on the board
 
+    //constructor
     public Ship(String name, int type) {
         this.type = type;
         this.name = name;
     }
 
+    //function to get the type
     public int getType() {
         return type;
     }
 
+    //function to set the type
     public void setType(int type) {
         this.type = type;
     }
 
+    //function to get name of the ship
     public String getName() {
         return name;
     }
 
+    //function to set name of the ship
     public void setName(String name) {
         this.name = name;
     }
 
+    //function to get list of ship positions
     public List<String> getPositions() {
         return positions;
     }
 
+    //function to set list of ship positions
     public void setPositions(List<String> positions) {
         this.positions = positions;
     }
