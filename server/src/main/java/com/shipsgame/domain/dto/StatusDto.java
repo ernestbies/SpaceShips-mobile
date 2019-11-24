ff4f86dde58935853d9725f21a825f5d4009f3a1
ernestbies <ernestbies@gmail.com>
2019-11-24 11:24:53 +0100

    Added descriptions to methods in class StatusDto.java

--- server/src/main/java/com/shipsgame/domain/dto/StatusDto.java
+++ server/src/main/java/com/shipsgame/domain/dto/StatusDto.java
@@ -1,12 +1,13 @@
 package com.shipsgame.domain.dto;
 
 public class StatusDto {
-    private int steps;
-    private String code;
-    private String shipName;
-    private String board;
-    private int type;
+    private int steps; //number of user steps
+    private String code; //code of game (for e.g. new game, end game)
+    private String shipName; //name of the spaceship
+    private String board; //current board status
+    private int type; //type of spaceship
 
+    //constructor
     public StatusDto(String code, String shipName, int type, int steps, String board){
         this.code = code;
         this.shipName = shipName;
@@ -15,22 +16,27 @@ public class StatusDto {
         this.board = board;
     }
 
+    //function to get number of steps
     public int getSteps() {
         return steps;
     }
 
+    //function to get game code
     public String getCode() {
         return code;
     }
 
+    //function to get ship name
     public String getShipName() {
         return shipName;
     }
 
+    //function to get type of ship
     public int getType() {
         return type;
     }
 
+    //function to get current board status
     public String getBoard() {
         return board;
     }
