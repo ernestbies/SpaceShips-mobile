6155db999aee955c5704d0fb56a2f4bd526919ea
Konrad Czechowski <k.czechowski@interia.eu>
2019-12-08 20:30:03 +0100

    Changed API endpoints

--- client/app/src/main/java/com/space/ships/JsonPlaceholderAPI.java
+++ client/app/src/main/java/com/space/ships/JsonPlaceholderAPI.java
@@ -2,15 +2,16 @@ package com.space.ships;
 
 import retrofit2.Call;
 import retrofit2.http.GET;
+import retrofit2.http.Path;
 import retrofit2.http.Query;
 
 public interface JsonPlaceholderAPI {
 
-    @GET("api/newgame")
-    Call<ServerResponse> newgame(@Query("user") String user);
+    @GET("api/newgame/{user}")
+    Call<ServerResponse> newgame(@Path("user") String user);
 
-    @GET("api/getgame")
-    Call<ServerResponse> getgame(@Query("user") String user);
+    @GET("api/getgame/{user}")
+    Call<ServerResponse> getgame(@Path("user") String user);
 
     @GET("api/shotgame")
     Call<ServerResponse> shotgame(@Query("user") String user, @Query("shot") String shot);
